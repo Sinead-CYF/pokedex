a18d4228ef994fafd78137ba2d90c0a2c77e3a28
Sinead <77743497+Sinead-CYF@users.noreply.github.com>
2021-11-13 21:55:28 +0000

    Added img, title and other components.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+
+const Logo = () => {
+  const appName = "Pokedex";
+  return (
+    <header>
+      <h1>Welcome to the {appName}</h1>;
+      <img
+        src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"
+        alt="Pokemon"
+      ></img>
+      ;
+    </header>
+  );
+};
+
+const FavPokemon = () => {
+  return <p>My favorite Pokemon is Squirtle</p>;
+};
+
+const CaughtPokemon = () => {
+  const date = new Date().toLocaleDateString();
+  return <p>Caught 0 Pokemon on {date}</p>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <FavPokemon />
+      <CaughtPokemon />
     </div>
   );
 }
